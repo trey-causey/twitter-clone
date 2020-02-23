bd80e759d82f9f2d4ab078506727fef07cf242b4
trey-causey <trey.causey@outlook.com>
2020-02-23 09:45:51 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let number = 20;
+let message = 'Thank you for using the twitter clone';
+let message1 = message.italics();
+function showMsg(message1) {
+
+    if(number == 20 )
+    document.write(`The message is: ${message1}`);
+}
+
+console.log(message1);
